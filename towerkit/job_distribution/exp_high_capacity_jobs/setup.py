361804bd57b9b42f26a0ec00500ed228f75523c4
chris meyers <chris.meyers.fsu@gmail.com>
2018-05-22 18:11:44 -0400

    validating new tower 3.3. algo

--- towerkit/job_distribution/exp_high_capacity_jobs/setup.py
+++ towerkit/job_distribution/exp_high_capacity_jobs/setup.py
@@ -15,6 +15,18 @@ sleep_max: 20
 #sleep_min: 240
 #sleep_max: 480
 
+def determine_capacity(v2):
+    for i in v2.instances.get()['results']:
+        if i.capacity > 0:
+            return i.capacity
+    return None
+
+forks = determine_capacity(v2)
+if forks is None:
+    raise RuntimeException("Capactiy not determinable")
+forks = forks - 1
+print("Forks set to {}".format(forks))
+
 cancel_all(v2.jobs)
 cancel_all(v2.workflow_jobs)
 
@@ -27,12 +39,12 @@ delete_all(v2.job_templates)
 org = get_or_create(v2.organizations, name='cmurders murder inc.')
 inv = get_or_create(org.related.inventories, name='cmurders murdertown', organization=org, variables=inventory_vars)
 
-for i in xrange(0, 100):
+for i in xrange(0, forks+5):
     h = get_or_create(inv.related.hosts, name='high-capacity-host-{}'.format(i), inventory=inv)
 
 proj = get_or_create(v2.projects, name='Murder Tools', organization=org, scm_url='https://github.com/chrismeyersfsu/ansible-examples.git', wait=True)
 
-jt = get_or_create(v2.job_templates, name="High Capacity JT - 19 forks", organization=org, inventory=inv, project=proj, extra_vars=sleep_interval_vars, playbook='sleep.yml', allow_simultaneous=True, forks=19)
+jt = get_or_create(v2.job_templates, name="High Capacity JT - {} forks".format(forks), organization=org, inventory=inv, project=proj, extra_vars=sleep_interval_vars, playbook='sleep.yml', allow_simultaneous=True, forks=forks)
 
 wfjt = get_or_create(v2.workflow_job_templates, name="High Capacity Workflow", organization=org)
 
